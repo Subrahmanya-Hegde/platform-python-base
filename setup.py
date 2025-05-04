5249b73f3175344de5fcfd79ba4e7b9c56f7989e
Subrahmanya Hegde <subrahmanyahegde@hegde.local>
2025-05-04 12:04:20 +0530

    Updated the requirements and setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from setuptools import setup, find_packages
 
+def load_requirements(filename="requirements.txt"):
+    with open(filename) as f:
+        return [line.strip() for line in f if line.strip() and not line.startswith("#")]
+
 setup(
     name="platform-python-base",
     version="0.1.0",
@@ -7,12 +11,6 @@ setup(
     author="Subrahmanya Hegde",
     author_email="subbu.v.h.234@gmail.com",
     packages=find_packages(exclude=("tests", "logs")),
-    install_requires=[
-        "fastapi>=0.100.0",
-        "pydantic>=1.10.0",
-        "python-dotenv>=1.0.0",
-        "pyyaml>=6.0",
-        "aiobreaker>=0.4.0"
-    ],
+    install_requires=load_requirements(),
     python_requires=">=3.8",
 )
